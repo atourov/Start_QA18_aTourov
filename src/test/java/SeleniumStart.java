fd298772519ece789eb838f70b10cdfcf3e6ff00
atourov <atourov@gmail.com>
2023-03-02 09:58:51 +0300

    HW_5

--- src/test/java/SeleniumStart.java
+++ src/test/java/SeleniumStart.java
@@ -16,39 +16,34 @@ public class SeleniumStart {
     public void preCondition() {
         System.out.println("Open website");
         wd = new ChromeDriver();
-//        wd.get("https://telranedu.web.app/home");
-        wd.navigate().to("https://ilcarro.web.app/search");
+        wd.get("https://telranedu.web.app/login");
+//        wd.navigate().to("c");
 //        wd.navigate().forward();
 //        wd.navigate().back();
-//        wd.findElement(By.className("container"));
 
     }
 
     @Test
     public void testLogin() {
 //        System.out.println("Test is over");
-        WebElement element = wd.findElement(By.tagName("a"));
+        WebElement element = wd.findElement(By.tagName("script"));
+        WebElement phoneBookID = wd.findElement(By.id("root"));
+        WebElement phoneBookClass1 = wd.findElement(By.className("container"));
+
+
         List<WebElement> elements = wd.findElements(By.tagName("a"));
         System.out.println(elements.size());
 
-        List<WebElement> elCarroClass = wd.findElements(By.className("navigation-link"));
-        System.out.println(elCarroClass.size());
-//        List<WebElement> phoneBookClass = wd.findElements(By.className("active"));
-//        System.out.println(phoneBookClass.size());
-//
-//        WebElement phoneBookClass1 = wd.findElement(By.className("container"));
-//
-//        List<WebElement> phoneBookLinkedText = wd.findElements(By.linkText("LOGIN"));
-//        System.out.println(phoneBookLinkedText.size());
-//
-//        List<WebElement> phoneBookPartial = wd.findElements(By.partialLinkText("O"));
-//        System.out.println(phoneBookPartial.size());
-//
-//        WebElement phoneBookID = wd.findElement(By.id("root"));
-//        System.out.println(phoneBookID);
+        List<WebElement> phoneBookClass = wd.findElements(By.className("active"));
+        System.out.println(phoneBookClass.size());
 
-        List<WebElement> elCarroID = wd.findElements(By.id("1"));
-        System.out.println(elCarroID.size());
+        List<WebElement> phoneBookLinkedText = wd.findElements(By.linkText("LOGIN"));
+        System.out.println(phoneBookLinkedText.size());
+
+        List<WebElement> phoneBookPartial = wd.findElements(By.partialLinkText("O"));
+        System.out.println(phoneBookPartial.size());
+
+
 
 
 
@@ -56,11 +51,6 @@ public class SeleniumStart {
 
     @AfterMethod
     public void postCondition() {
-//        wd.close();
-//        wd.quit();
-//        WebElement element = wd.findElement(By.tagName("a"));
-//        List<WebElement> elements = (List<WebElement>) wd.findElement(By.tagName("a"));
-//        System.out.println(elements.size());
 
     }
 }
